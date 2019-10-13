fa04f7f42bc0e0b61f527a82051e8082c5163e15
ityard <946444345@qq.com>
2019-10-13 13:21:37 +0800

    使用 selenium 登录 163 邮箱

---
+++ selenium-163-demo/demo1.py
@@ -0,0 +1,24 @@
+'''
+instruction：登录 163 邮箱示例
+author：程序员野客
+'''
+
+# 需先将二维码登录方式切换到用户名密码登录方式
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('https://mail.163.com/')
+# 将扫码登录转切换为用户名密码登录
+browser.find_element_by_xpath('//div[@id="lbNormal"]').click()
+# 转化登录方式之后，要进入iframe框
+browser.switch_to.frame(browser.find_element_by_xpath('//iframe[starts-with(@id,"x-URS")]'))
+browser.implicitly_wait(2)
+# 自己的用户名
+browser.find_element_by_xpath('//input[@name="email"]').send_keys('xxx')
+# 自己的密码
+browser.find_element_by_xpath('//input[@name="password"]').send_keys('xxx')
+browser.find_element_by_xpath('//*[@id="dologin"]').click()
+browser.implicitly_wait(2)
+print(browser.page_source)
+browser.implicitly_wait(2)
+browser.quit()
---
+++ selenium-163-demo/demo2.py
@@ -0,0 +1,21 @@
+'''
+instruction：登录 163 邮箱示例
+author：程序员野客
+'''
+
+# 直接通过用户名、密码登录
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('https://email2.163.com/')
+browser.switch_to.frame(browser.find_element_by_xpath('//iframe[starts-with(@id,"x-URS")]'))
+browser.implicitly_wait(2)
+# 自己的用户名
+browser.find_element_by_xpath('//input[@name="email"]').send_keys('xxx')
+# 自己的密码
+browser.find_element_by_xpath('//input[@name="password"]').send_keys('xxx')
+browser.find_element_by_xpath('//*[@id="dologin"]').click()
+browser.implicitly_wait(2)
+print(browser.page_source)
+browser.implicitly_wait(2)
+browser.quit()
