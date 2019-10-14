c3045f7b17deee903c3d042e23e886b94ac88469
程序员野客 <946444345@qq.com>
2019-10-14 20:25:48 +0800

    使用 selenium 登录 163 邮箱

--- selenium-163-demo/demo2.py
+++ selenium-163-demo/demo2.py
@@ -7,18 +7,16 @@ author：程序员野客
 from selenium import webdriver
 
 browser = webdriver.Chrome()
+browser.implicitly_wait(2)
 browser.get('https://mail.163.com/')
 # 将扫码登录转切换为用户名密码登录
 browser.find_element_by_xpath('//div[@id="lbNormal"]').click()
 # 转化登录方式之后，要进入iframe框
 browser.switch_to.frame(browser.find_element_by_xpath('//iframe[starts-with(@id,"x-URS")]'))
-browser.implicitly_wait(2)
 # 自己的用户名
 browser.find_element_by_xpath('//input[@name="email"]').send_keys('xxx')
 # 自己的密码
 browser.find_element_by_xpath('//input[@name="password"]').send_keys('xxx')
 browser.find_element_by_xpath('//*[@id="dologin"]').click()
-browser.implicitly_wait(2)
 print(browser.page_source)
-browser.implicitly_wait(2)
 browser.quit()
